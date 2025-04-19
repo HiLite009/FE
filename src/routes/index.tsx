c534a3efb120aad0214f437b178030dd4c453ca2
easyhak <jooin2000@cau.ac.kr>
2025-04-19 20:36:24 +0900

    Delete: 필요 없는 파일 삭제

--- src/routes/index.tsx
+++
@@ -1,9 +0,0 @@
-// import { Routes, Route, Navigate } from 'react-router-dom';
-// import LoginPage from '../pages/login/LoginPage.tsx';
-// import ChatPage from '../pages/chat/ChatPage.tsx';
-//
-// export const AppRoutes = () => {
-//   return (
-//
-//   );
-// };
