3e62453f101fc2536a75d6a3446883046e389363
easyhak <jooin2000@cau.ac.kr>
2025-04-21 23:47:27 +0900

    Fix: lint 관련 설정 수정

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
 import js from '@eslint/js'
-import tsEslint from "@typescript-eslint/eslint-plugin"; // plugin 객체
-import tsParser from "@typescript-eslint/parser"; // parser
+import tsEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
@@ -8,12 +8,15 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 export default [
   { ignores: ['dist'] },
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        React: true
+      },
       parser: tsParser,
-      node: true,
       parserOptions: {
         ecmaVersion: '2022',
         ecmaFeatures: { jsx: true },
@@ -30,7 +33,11 @@ export default [
       ...reactHooks.configs.recommended.rules,
       "react-hooks/rules-of-hooks": "error",
       "react-hooks/exhaustive-deps": "warn",
-      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["warn", {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+      }],
     },
   },
 ]
